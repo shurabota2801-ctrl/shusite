562e6043e741c3e5bcd248b85720201c724e34cf
Shurabota <shurabota2801@gmail.com>
2025-08-14 22:11:18 +0500

    first commit

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class LoginView(generics.GenericAPIView):
+    permission_classes = (permissions.AllowAny,)
+    
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        return Response({'error': 'Invalid credentials'}, status=400)
